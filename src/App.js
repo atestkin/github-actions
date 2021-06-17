9ee0ffda264345ff7a29001ca7d957b1cbe6f8b2
Amrit <amritleensingh.singh@telus.com>
2021-06-17 09:04:27 -0700

    updated welcome msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello this is awesome
+          Edit <code>src/App.js</code> this is Mandeep and this is first branch.
         </p>
         <a
           className="App-link"
