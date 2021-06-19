6646dafb94aee928a03852928a07a207be4f91eb
atestkin <36210098+atestkin@users.noreply.github.com>
2021-06-19 08:05:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> this is Mandeep and this is second branch. I am learning git. this is crazy
+          Edit <code>src/App.js</code> this is Mandeep and this is second branch. I am learning git. this is crazy...wow
         </p>
         <a
           className="App-link"
